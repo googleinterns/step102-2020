2bc8ab502d6437e09fe3ce8baef416b8144a9c2e
Keely Wan <keelywan00@gmail.com>
2020-07-08 21:05:04 +0000

    Add error message

--- src/main/webapp/components/search-bar.vue
+++ src/main/webapp/components/search-bar.vue
@@ -1,26 +1,29 @@
 <template>
   <form class="flex" id="search-container" action="/" @submit="checkForm">
-    <div class="flex" id="search-area">
-      <div class="flex search-item">
-        <label for="school">School:</label>
-        <input
-            type="text"
-            id="school"
-            name="school"
-            v-model.trim="school"
-            placeholder="Cambridge University">
+    <div class="flex">
+      <div class="flex" id="search-area">
+        <div class="flex search-item">
+          <label for="school">School:</label>
+          <input
+              type="text"
+              id="school"
+              name="school"
+              v-model.trim="school"
+              placeholder="Cambridge University">
+        </div>
+        <div class="flex search-item">
+          <label for="course">Course:</label>
+          <input
+              type="text"
+              id="course"
+              name="course"
+              v-model.trim="course"
+              placeholder="CS4410">
+        </div>
       </div>
-      <div class="flex search-item">
-        <label for="course">Course:</label>
-        <input
-            type="text"
-            id="course"
-            name="course"
-            v-model.trim="course"
-            placeholder="CS4410">
-      </div>
+      <input type="submit" value="Search" id="search-btn">
     </div>
-    <input type="submit" value="Search" id="search-btn">
+    <p class="red" v-if="error">{{ error }}</p>
   </form>
 </template>
 
@@ -29,14 +32,18 @@ module.exports = {
   data: function() {
     return {
       school: null,
-      course: null
+      course: null,
+      error: null
     }
   },
   methods: {
     checkForm: function(e) {
       if(!this.school && !this.course) {
         e.preventDefault();
-        // TODO: Add error message on form
+        this.error = "Please input either a school or course code.";
+      }
+      else {
+        this.error = "";
       }
     }
   }
@@ -48,11 +55,21 @@ input {
   font-size: inherit;
 }
 
+p {
+  margin: 0;
+}
+
 .flex {
   display: flex;
 }
 
+.red {
+  color: red;
+}
+
 #search-container {
+  align-items: center;
+  flex-direction: column;
   font-size: 1.5rem;
   justify-content: center;
   width: 100%;
