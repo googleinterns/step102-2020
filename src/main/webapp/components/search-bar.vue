5a0462279477d4d9ba00492b4f5299df1a0a4feb
Keely Wan <keelywan00@gmail.com>
2020-07-08 14:42:42 +0000

    Add form validation

--- src/main/webapp/components/search-bar.vue
+++ src/main/webapp/components/search-bar.vue
@@ -1,18 +1,47 @@
 <template>
-  <form class="flex" id="search-container" action="/">
+  <form class="flex" id="search-container" action="/" @submit="checkForm">
     <div class="flex" id="search-area">
       <div class="flex search-item">
         <label for="school">School:</label>
-        <input type="text" id="school" name="school" placeholder="Cambridge University">
+        <input
+            type="text"
+            id="school"
+            name="school"
+            v-model.trim="school"
+            placeholder="Cambridge University">
       </div>
       <div class="flex search-item">
         <label for="course">Course:</label>
-        <input type="text" id="course" name="course" placeholder="CS4410">
+        <input
+            type="text"
+            id="course"
+            name="course"
+            v-model.trim="course"
+            placeholder="CS4410">
       </div>
     </div>
     <input type="submit" value="Search" id="search-btn">
   </form>
 </template>
+
+<script>
+module.exports = {
+  data: function() {
+    return {
+      school: null,
+      course: null
+    }
+  },
+  methods: {
+    checkForm: function(e) {
+      if(!this.school && !this.course) {
+        e.preventDefault();
+      }
+      return true;
+    }
+  }
+}
+</script>
 
 <style scoped>
 input {
