67bed675b1b4eca370a26c19deefc05da1d3c749
Andres David <andres.holguin@ufl.edu>
2020-07-08 13:31:11 -0400

    Create LabelList.vue
    Create a basic component that contains and displays the labels for a note.
    Props:
     - canModify: Boolean
    Whether the icons for removing, adding, and replacing labels appear
    
    - school: Object
    The object containing the data for the required school label
    
    - course: Object
    The object containing the data for the required course label
    
    - labels: Array
    All the remaining labels that have been submitted by users.

---
+++ src/main/webapp/components/LabelList.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="labels-list">
+    <!-- School Label -->
+    <v-chip
+      class="ma-1"
+      color="orange"
+      dark label
+      :close="canModify"
+      close-icon='mdi-find-replace'
+    >
+      <v-avatar
+        left
+        color="orange darken-3"
+      >
+        {{school.code}}
+      </v-avatar>
+      {{school.name}}
+    </v-chip>
+
+    <!-- Course Label -->
+    <v-chip
+      class="ma-1"
+      color="blue"
+      dark label
+      :close="canModify"
+      close-icon='mdi-find-replace'
+    >
+      <v-icon
+        left x-small
+      >
+        {{course.code}}
+      </v-icon>
+      {{course.name}}
+    </v-chip>
+
+    <!-- Remaining Labels -->
+    <v-chip 
+      v-for="label in labels"
+      :key="label"
+      class="ma-1"
+      label
+      :close="canModify"
+      close-icon='mdi-delete'
+    >
+      {{label}}
+    </v-chip>
+
+    <v-chip
+      v-if="canModify"
+      label
+    >
+      <v-icon>
+        mdi-plus
+      </v-icon>
+    </v-chip>
+  </div>  
+</template>
+
+<script>
+module.exports = {
+  props: {
+    canModify: {
+      type: Boolean,
+      default: function() {
+        return false;
+      }
+    },
+    school: {
+      type: Object,
+      default: function() {
+        return {
+          code: 'N/A',
+          name: 'Unaffiliated'
+        }
+      }
+    },
+    course: {
+      type: Object,
+      default: function() {
+        return {
+          code: 'N/A',
+          name: 'Unaffiliated'
+        }
+      }
+    },
+    labels: Array
+  }
+}
+</script>
