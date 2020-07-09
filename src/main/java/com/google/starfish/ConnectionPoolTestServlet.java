e4e9004ac10ade1f7989896f707272bd440a9bae
Aradhya Bansal <aradhyabansal2000@gmail.com>
2020-07-09 19:01:17 +0000

    Convert tabs into spaces in Java files for formatting purposes

--- src/main/java/com/google/starfish/ConnectionPoolTestServlet.java
+++ src/main/java/com/google/starfish/ConnectionPoolTestServlet.java
@@ -16,14 +16,14 @@ import javax.sql.DataSource;
  */  
 @WebServlet("/test-pool-connection")  
 public class ConnectionPoolTestServlet extends HttpServlet {  
-	@Override
-	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		PrintWriter out = res.getWriter();  
-		DataSource pool = (DataSource) req.getServletContext().getAttribute("my-pool");  
-		try (Connection conn = pool.getConnection()) {
-			if (conn != null) res.getWriter().println("Database is connected!");
-		} catch (SQLException ex) {
-				res.getWriter().println("Database is not connected");  
-		}
-	}  
+  @Override
+  public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+    PrintWriter out = res.getWriter();  
+    DataSource pool = (DataSource) req.getServletContext().getAttribute("my-pool");  
+    try (Connection conn = pool.getConnection()) {
+      if (conn != null) res.getWriter().println("Database is connected!");
+    } catch (SQLException ex) {
+        res.getWriter().println("Database is not connected");  
+    }
+  }  
 }  
