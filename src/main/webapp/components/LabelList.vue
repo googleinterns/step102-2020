70a20a8662b5a766ececed67d87844d92fb0ed6d
Andres David <andres.holguin@ufl.edu>
2020-07-09 20:23:08 -0400

    Update LabelList.vue
    Update to make tags more legible based on PR Review.

--- src/main/webapp/components/LabelList.vue
+++ src/main/webapp/components/LabelList.vue
@@ -1,54 +1,42 @@
 <template>
   <div class="labels-list">
     <!-- School Label -->
-    <v-chip
-      class="ma-1"
-      color="orange"
-      dark label
-      :close="canModify"
-      close-icon='mdi-find-replace'
-    >
-      <v-avatar
-        left
-        color="orange darken-3"
-      >
+    <v-chip dark label
+            class="ma-1"
+            color="orange"
+            :close="canModify"
+            close-icon='mdi-find-replace'>
+      <v-avatar left
+                color="orange darken-3">
         {{school.code}}
       </v-avatar>
       {{school.name}}
     </v-chip>
 
     <!-- Course Label -->
-    <v-chip
-      class="ma-1"
-      color="blue"
-      dark label
-      :close="canModify"
-      close-icon='mdi-find-replace'
-    >
-      <v-icon
-        left x-small
-      >
+    <v-chip dark label
+            class="ma-1"
+            color="blue"
+            :close="canModify"
+            close-icon='mdi-find-replace'>
+      <v-icon left x-small>
         {{course.code}}
       </v-icon>
       {{course.name}}
     </v-chip>
 
     <!-- Remaining Labels -->
-    <v-chip 
-      v-for="label in labels"
-      :key="label"
-      class="ma-1"
-      label
-      :close="canModify"
-      close-icon='mdi-delete'
-    >
+    <v-chip label
+            class="ma-1"
+            v-for="label in labels"
+            :key="label"
+            :close="canModify"
+            close-icon='mdi-delete'>
       {{label}}
     </v-chip>
 
-    <v-chip
-      v-if="canModify"
-      label
-    >
+    <v-chip label
+            v-if="canModify">
       <v-icon>
         mdi-plus
       </v-icon>
