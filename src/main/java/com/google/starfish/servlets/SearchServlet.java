eef24d95aab2f0723b8bb5bd5c03b0bb9cf8f042
Aradhya Bansal <aradhyabansal2000@gmail.com>
2020-07-14 19:59:32 +0000

    Complete first implementation of search servlet

--- src/main/java/com/google/starfish/servlets/SearchServlet.java
+++ src/main/java/com/google/starfish/servlets/SearchServlet.java
@@ -1,7 +1,6 @@
 package com.google.starfish.servlets;  
 
 import java.io.IOException;  
-import java.io.PrintWriter;  
 import java.sql.Connection;  
 import java.sql.SQLException;  
 import java.sql.PreparedStatement;
@@ -12,7 +11,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;  
 import javax.servlet.http.HttpServletResponse;  
 import javax.sql.DataSource;
+import java.util.Date;
 import com.google.starfish.services.NoteService;
+import com.google.starfish.models.Note;
+import com.google.gson.Gson;
+import java.util.ArrayList;
 
 /** Servlet that checks the status of open database connection pool in the app context. */  
 @WebServlet("/search")  
@@ -23,23 +26,55 @@ public class SearchServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
     DataSource pool = (DataSource) req.getServletContext().getAttribute("my-pool");  
-    String school = req.getParameter("school");
-    String course = req.getParameter("course");
+    String reqSchool = req.getParameter("school");
+    String reqCourse= req.getParameter("course");
+    ArrayList<Note> notes = new ArrayList<>();
     try (Connection conn = pool.getConnection()) {
       String stmt = 
           "SELECT * "
         + "FROM `starfish.notes` "
-        + "WHERE `school`='" + school + "' AND `course`='" + course + "';";
+        + "WHERE `school`= ? AND `course` = ?;";
       try (PreparedStatement getNotesStatement = conn.prepareStatement(stmt)) {
+        getNotesStatement.setString(1, reqSchool);
+        getNotesStatement.setString(2, reqCourse);
         ResultSet rs = getNotesStatement.executeQuery();
 
         while (rs.next()) {
+          long noteId = rs.getLong("id");
+          long authorId = rs.getLong("author_id");
+          String school = rs.getString("school");
+          String course = rs.getString("course");
+          String title = rs.getString("title");
+          String sourceUrl = rs.getString("title");
+          String pdfSource = rs.getString("pdf_source");
+          Date dateCreated = rs.getDate("date_created");
+          long numDownloads = rs.getLong("num_downloads");
+          long numFavorites = noteService.getNumFavoritesById(pool, noteId);
 
+          Note thisNote = new Note.Builder()
+                              .setId(noteId)
+                              .setAuthorId(authorId)
+                              .setRequiredLabels(school, course)
+                              .setNoteTitle(title)
+                              .setSourceUrl(sourceUrl)
+                              .setOptionalPdfSource(pdfSource)
+                              .setDateCreated(dateCreated)
+                              .setNumDownloads(numDownloads)
+                              .setNumFavorites(numFavorites)
+                              .build();
+          notes.add(thisNote);
         }
+        String json = convertArrayListToJson(notes);
+        res.setContentType("application/json");
+        res.getWriter().println(json);
       }
-
     } catch (SQLException ex) {
-        res.getWriter().println("Database is not connected");  
+        res.getWriter().println("Error caught while speaking to database: " + ex);  
     }
   }  
+
+  private String convertArrayListToJson(ArrayList<Note> notes) {
+    Gson gson = new Gson();
+    return gson.toJson(notes);
+  }
 }  
