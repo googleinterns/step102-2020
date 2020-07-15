9d30abb2ac0d9efd284a741cb0bb24243b410ea0
Aradhya Bansal <aradhyabansal2000@gmail.com>
2020-07-15 14:58:13 +0000

    Add user service class

---
+++ src/main/java/com/google/starfish/services/UserService.java
@@ -0,0 +1,109 @@
+package com.google.starfish.services;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;  
+import java.sql.ResultSet;  
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.Date;
+import com.google.starfish.models.Note;
+
+public class UserService extends GenericService {
+
+  NoteService noteService = new NoteService();
+
+  public ResultSet getById(DataSource pool, long id) throws SQLException {
+    return super.getById(pool, id, Table.USERS);
+  }
+
+  public boolean deleteById(DataSource pool, long id) throws SQLException {
+    return super.deleteById(pool, id, Table.USERS);
+  }
+
+  public Note[] getFavoriteNotesById(DataSource pool, long userId) throws SQLException {
+    ArrayList<Note> notes = new ArrayList<>();
+    try (Connection conn = pool.getConnection()) {
+      String stmt = 
+          "SELECT  a.* "
+        + "FROM "
+          + Table.NOTES + " AS a "
+          + "INNER JOIN (SELECT * "
+                      + "FROM " + Table.FAVORITE_NOTES + " "
+                      + "WHERE user_id=?) "
+          + "as b ON a.id=b.note_id;";
+      try (PreparedStatement favNotesStmt = conn.prepareStatement(stmt)) {
+        favNotesStmt.setLong(1, userId);
+        ResultSet rs = favNotesStmt.executeQuery();
+        while (rs.next()) {
+          long noteId = rs.getLong("id");
+          long authorId = rs.getLong("author_id");
+          String school = rs.getString("school");
+          String course = rs.getString("course");
+          String title = rs.getString("title");
+          String sourceUrl = rs.getString("title");
+          String pdfSource = rs.getString("pdf_source");
+          Date dateCreated = rs.getDate("date_created");
+          long numDownloads = rs.getLong("num_downloads");
+          long numFavorites = noteService.getNumFavoritesById(pool, noteId);
+
+          Note thisNote = new Note.Builder()
+                              .setId(noteId)
+                              .setAuthorId(authorId)
+                              .setRequiredLabels(school, course)
+                              .setNoteTitle(title)
+                              .setSourceUrl(sourceUrl)
+                              .setOptionalPdfSource(pdfSource)
+                              .setDateCreated(dateCreated)
+                              .setNumDownloads(numDownloads)
+                              .setNumFavorites(numFavorites)
+                              .build();
+          notes.add(thisNote);
+        }
+        rs.close();
+        return notes.toArray(new Note[0]);
+      }
+    }
+  }
+
+  public Note[] getUploadedNotesById(DataSource pool, long userId) throws SQLException {
+    ArrayList<Note> notes = new ArrayList<>();
+    try (Connection conn = pool.getConnection()) {
+      String stmt = 
+          "SELECT * "
+        + "FROM " + Table.NOTES + " "
+        + "WHERE author_id=?;";
+      try (PreparedStatement upldNotesStmt = conn.prepareStatement(stmt)) {
+        upldNotesStmt.setLong(1, userId);
+        ResultSet rs = upldNotesStmt.executeQuery();
+        while (rs.next()) {
+          long noteId = rs.getLong("id");
+          long authorId = rs.getLong("author_id");
+          String school = rs.getString("school");
+          String course = rs.getString("course");
+          String title = rs.getString("title");
+          String sourceUrl = rs.getString("title");
+          String pdfSource = rs.getString("pdf_source");
+          Date dateCreated = rs.getDate("date_created");
+          long numDownloads = rs.getLong("num_downloads");
+          long numFavorites = noteService.getNumFavoritesById(pool, noteId);
+
+          Note thisNote = new Note.Builder()
+                              .setId(noteId)
+                              .setAuthorId(authorId)
+                              .setRequiredLabels(school, course)
+                              .setNoteTitle(title)
+                              .setSourceUrl(sourceUrl)
+                              .setOptionalPdfSource(pdfSource)
+                              .setDateCreated(dateCreated)
+                              .setNumDownloads(numDownloads)
+                              .setNumFavorites(numFavorites)
+                              .build();
+          notes.add(thisNote);
+        }
+        rs.close();
+        return notes.toArray(new Note[0]);
+      }
+    }
+  }
+}
