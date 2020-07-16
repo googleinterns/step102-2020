ae4e83715676fe923d63f055ebbe459d1d95ab1f
Aradhya Bansal <aradhyabansal2000@gmail.com>
2020-07-16 23:13:11 +0000

    Change upload-notes path to handle-notes path

--- src/main/java/com/google/starfish/servlets/BlobstoreUploadUrlServlet.java
+++ src/main/java/com/google/starfish/servlets/BlobstoreUploadUrlServlet.java
@@ -14,7 +14,7 @@ public class BlobstoreUploadUrlServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
-    String uploadUrl = blobstoreService.createUploadUrl("/upload-notes");
+    String uploadUrl = blobstoreService.createUploadUrl("/handle-notes");
 
     response.setContentType("text/html");
     response.getWriter().println(uploadUrl);
