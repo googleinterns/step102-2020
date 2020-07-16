532c54d31726cd4004836dbcca3a1ee55fb6190c
Aradhya Bansal <aradhyabansal2000@gmail.com>
2020-07-16 23:13:39 +0000

    Add database interfacing to add notes and labels from the HandleNotesServlet to DB

--- src/main/java/com/google/starfish/servlets/HandleNotesServlet.java
+++ src/main/java/com/google/starfish/servlets/HandleNotesServlet.java
@@ -1,4 +1,4 @@
-package com.google.servlets;
+package com.google.starfish.servlets;
 
 import com.google.appengine.api.blobstore.BlobInfo;
 import com.google.appengine.api.blobstore.BlobInfoFactory;
@@ -17,18 +17,35 @@ import java.util.Map;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;  
 import javax.servlet.http.Cookie;  
+import com.google.starfish.services.NoteService;
+import com.google.starfish.services.LabelService;
+import com.google.starfish.services.MiscNoteLabelService;
+import javax.sql.DataSource;
+import java.sql.Connection;  
+import java.sql.SQLException;  
+import java.sql.PreparedStatement;
+import java.sql.Types;  
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.Date;
+import java.util.Calendar;
+
 
 @WebServlet("/handle-notes")
 public class HandleNotesServlet extends HttpServlet {
 
   private BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
   private final String COOKIE_NAME = "SFCookie";
+  private NoteService noteService = new NoteService();
+  private LabelService labelService = new LabelService();
+  private MiscNoteLabelService miscNoteLabelService = new MiscNoteLabelService();
   
   @Override
-  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     
     if(!validateUser(request)) {
       response.setStatus(HttpServletResponse.SC_FORBIDDEN);
@@ -39,14 +56,76 @@ public class HandleNotesServlet extends HttpServlet {
     String userId = (String) activeSession.getAttribute("user_id");
 
     String blobKey = getUploadedFileBlobKey(request, "file");
+    if (blobKey == null) {
+      response.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
+      return;
+    }
     String title = request.getParameter("title");
     String school = request.getParameter("school");
     String course = request.getParameter("course");
-    // TODO: Not sure how the labels will be separated, will need to parse into array of strings
-    String miscLabels = request.getParameter("miscLabels");
+    String[] miscLabels = request.getParameter("miscLabels").split(",");
 
-    // TODO: Put params into database
+    // TODO: Make SQL queries not auto commit and rollback transaction on catching exception
+    
+    DataSource pool = (DataSource) request.getServletContext().getAttribute("my-pool");
+    
+    try (Connection conn = pool.getConnection()) {
+      String stmt =
+          "INSERT INTO notes ( "
+              + "author_id,"
+              + "school,"
+              + "course,"
+              + "title,"
+              + "source_url,"
+              + "pdf_source,"
+              + "date_created,"
+              + "num_downloads ) "
+        + "VALUES ( "
+              + "?,"
+              + "?,"
+              + "?,"
+              + "?,"
+              + "?,"
+              + "?,"
+              + "?,"
+              + "? ); ";
 
+      try (PreparedStatement noteStmt = conn.prepareStatement(stmt, Statement.RETURN_GENERATED_KEYS)) {
+        noteStmt.setString(1, userId);
+        noteStmt.setString(2, school);
+        noteStmt.setString(3, course);
+        noteStmt.setString(4, title);
+        noteStmt.setString(5, blobKey);
+        noteStmt.setString(6, blobKey);
+        noteStmt.setDate(7, new Date(Calendar.getInstance().getTimeInMillis()));
+        noteStmt.setLong(8, 0);
+        System.out.println("Posting note");
+        int rowsAffected = noteStmt.executeUpdate();
+        if (rowsAffected == 1) {
+          long noteId = 0;
+          ResultSet rs = noteStmt.getGeneratedKeys();
+          if (rs.next()) {
+            noteId = rs.getLong(1);
+            System.out.println("NoteId: " + noteId);
+
+            // Use label services to insert all new labels into the database
+            labelService.insertSchoolLabel(pool, school);
+            labelService.insertCourseLabel(pool, course);
+            for (String miscLabel : miscLabels) {
+              labelService.insertMiscLabel(pool, miscLabel);
+            }
+            for (String miscLabel : miscLabels) {
+              miscNoteLabelService.insertMiscNoteLabelById(pool, noteId, miscLabel);
+            }
+          } 
+        }
+      }
+    } catch (SQLException ex) {
+      // LOGGER.log(Level.WARNING, "Error while speaking to database:", ex);
+      // Set an error code of 500 if the server can't connect to the database
+      response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+      response.getWriter().println("INTERNAL SERVER ERROR: " + ex);
+    }
     response.sendRedirect("/");
   }
 
