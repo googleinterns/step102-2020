d114abd5f3eb3ff0792147b3d201a18ac3a4edbf
Aradhya Bansal <aradhyabansal2000@gmail.com>
2020-07-17 19:28:44 +0000

    Add method to get uploaded notes by user id

--- src/main/java/com/google/starfish/services/NoteService.java
+++ src/main/java/com/google/starfish/services/NoteService.java
@@ -5,6 +5,10 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;  
 import java.sql.ResultSet;  
 import javax.sql.DataSource;
+import java.util.List;
+import java.util.Date;
+import java.util.ArrayList;
+import com.google.starfish.models.Note;
 
 /**
  * Service class for Notes that allows retrieval of values stored as
@@ -32,6 +36,47 @@ public class NoteService extends TableService {
         long numFavorites = rs.getLong("num_favorites");
         rs.close();
         return numFavorites;
+      }
+    }
+  }
+
+  public Note[] getUploadedNotesByUserId(DataSource pool, String userId) throws SQLException {
+    List<Note> notes = new ArrayList<>();
+    try (Connection conn = pool.getConnection()) {
+      String stmt = 
+          "SELECT * "
+        + "FROM " + Table.NOTES + " "
+        + "WHERE author_id=?;";
+      try (PreparedStatement upldNotesStmt = conn.prepareStatement(stmt)) {
+        upldNotesStmt.setString(1, userId);
+        ResultSet rs = upldNotesStmt.executeQuery();
+        while (rs.next()) {
+          long noteId = rs.getLong("id");
+          String authorId = rs.getString("author_id");
+          String school = rs.getString("school");
+          String course = rs.getString("course");
+          String title = rs.getString("title");
+          String sourceUrl = rs.getString("title");
+          String pdfSource = rs.getString("pdf_source");
+          Date dateCreated = rs.getDate("date_created");
+          long numDownloads = rs.getLong("num_downloads");
+          long numFavorites = getNumFavoritesById(pool, noteId);
+
+          Note thisNote = new Note.Builder()
+                              .setId(noteId)
+                              .setAuthorId(authorId)
+                              .setRequiredLabels(school, course)
+                              .setNoteTitle(title)
+                              .setSourceUrl(sourceUrl)
+                              .setOptionalPdfSource(pdfSource)
+                              .setDateCreated(dateCreated)
+                              .setNumDownloads(numDownloads)
+                              .setNumFavorites(numFavorites)
+                              .build();
+          notes.add(thisNote);
+        }
+        rs.close();
+        return notes.toArray(new Note[0]);
       }
     }
   }
