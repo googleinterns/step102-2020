febbd2ea8b2ed4342e58bbdd07409c5a716578cc
Andres David <andres.holguin@ufl.edu>
2020-07-17 10:17:49 -0400

    Update Navbar.vue
    Remove custom directive, already set by Vuetify

--- src/main/webapp/components/Navbar.vue
+++ src/main/webapp/components/Navbar.vue
@@ -26,19 +26,6 @@
 </template>
 
 <script>
-Vue.directive('click-outside', {
-  bind: function (el, binding, vnode) {
-    this.event = function(event) {
-      if(!(el == event.target || el.contains(event.target))) {
-        vnode.context[binding.expression](event);
-      }
-    };
-    document.body.addEventListener('click', this.event);
-  },
-  unbind: function(el) {
-    document.body.removeEventListener('click', this.event);
-  }
-});
 
 module.exports = {
   components: {
