d732de90ecadca00160a5c2edf2ac7f868433f95
Aradhya Bansal <aradhyabansal2000@gmail.com>
2020-07-17 20:40:29 +0000

    Add overloaded get/delete by id methods to table service for usage with user service

--- src/main/java/com/google/starfish/services/TableService.java
+++ src/main/java/com/google/starfish/services/TableService.java
@@ -30,14 +30,14 @@ public class TableService {
     this.sqlTable = sqlTable;
   }
 
-  /** Gets row by id from sql */
+  /** Gets row by long id from sql */
   public ResultSet getRowById(DataSource pool, long id) {
     try (Connection conn = pool.getConnection()) {
       try {
         conn.setAutoCommit(false);
         String stmt = 
             "SELECT * "
-          + "FROM starfish." + this.sqlTable + " "
+          + "FROM " + this.sqlTable.getSqlTable() + " "
           + "WHERE `id`= ? "
           + "LIMIT 1;";
         try (PreparedStatement getStmt = conn.prepareStatement(stmt)) {
@@ -64,18 +64,86 @@ public class TableService {
     }
   }
 
-  /** Deletes row by id from sql */
+  /** Gets row by string id from sql */
+  public ResultSet getRowById(DataSource pool, String id) {
+    try (Connection conn = pool.getConnection()) {
+      try {
+        conn.setAutoCommit(false);
+        String stmt = 
+            "SELECT * "
+          + "FROM " + this.sqlTable.getSqlTable() + " "
+          + "WHERE `id`= ? "
+          + "LIMIT 1;";
+        try (PreparedStatement getStmt = conn.prepareStatement(stmt)) {
+          getStmt.setString(1, id);
+          ResultSet rs = getStmt.executeQuery();
+          conn.commit();
+          return rs;
+        } 
+      } catch (SQLException ex) {
+          if (conn != null) {
+            try {
+              System.err.print("Transaction is being rolled back.");
+              conn.rollback();
+            } catch (SQLException excep) {
+              System.err.print(excep);
+              return null;
+            }
+          }
+          return null;
+        }
+    } catch (SQLException ex) {
+      System.err.print(ex);
+      return null;
+    }
+  }
+
+  /** Deletes row by long id from sql */
   public boolean deleteRowById(DataSource pool, long id) {
     try (Connection conn = pool.getConnection()) {
       try {
         conn.setAutoCommit(false);
         String stmt = 
             "DELETE * "
-          + "FROM starfish." + this.sqlTable + " "
+          + "FROM " + this.sqlTable.getSqlTable() + " "
           + "WHERE `id`= ? "
           + "LIMIT 1;";
         try (PreparedStatement deleteStmt = conn.prepareStatement(stmt)) {
           deleteStmt.setLong(1, id);
+          boolean deleted = deleteStmt.execute();
+          conn.commit();
+          return deleted;
+        } 
+      } catch (SQLException ex) {
+          if (conn != null) {
+            try {
+              System.err.print("Transaction is being rolled back.");
+              conn.rollback();
+            } catch (SQLException excep) {
+              System.err.print(excep);
+              return false;
+            }
+          }
+          return false;
+        }
+    } catch (SQLException ex) {
+      System.err.print(ex);
+      return false;
+    }
+  }
+
+  /** Deletes row by string id from sql */
+  public boolean deleteRowById(DataSource pool, String id) {
+    try (Connection conn = pool.getConnection()) {
+      try {
+        conn.setAutoCommit(false);
+        String stmt = 
+            "DELETE * "
+          + "FROM starfish." + this.sqlTable.getSqlTable() + " "
+          + "WHERE `id`= ? "
+          + "LIMIT 1;";
+        try (PreparedStatement deleteStmt = conn.prepareStatement(stmt)) {
+          deleteStmt.setString(1, id);
           boolean deleted = deleteStmt.execute();
           conn.commit();
           return deleted;
