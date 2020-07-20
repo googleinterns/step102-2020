803b737283b1780738a26a0ecd8a4c162f770894
Keely Wan <keelywan00@gmail.com>
2020-07-20 20:50:44 +0000

    Create FavoriteNoteServlet

---
+++ src/main/java/com/google/starfish/servlets/FavoriteNoteServlet.java
@@ -0,0 +1,91 @@
+package com.google.starfish.servlets;  
+
+import java.io.IOException;  
+import java.sql.Connection;  
+import java.sql.SQLException;  
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.servlet.ServletException;  
+import javax.servlet.annotation.WebServlet;  
+import javax.servlet.http.HttpServlet;  
+import javax.servlet.http.HttpSession;  
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;  
+import javax.servlet.http.HttpServletResponse;  
+import javax.sql.DataSource;
+import java.util.Date;
+import com.google.starfish.services.FavoriteNoteService;
+import com.google.gson.Gson;
+import java.util.ArrayList;
+import java.util.List;
+
+/** Servlet that returns search results for notes based on school and course */
+@WebServlet("/favorite-note")  
+public class FavoriteNoteServlet extends HttpServlet {  
+
+  private FavoriteNoteService favoriteNoteService = new FavoriteNoteService();
+  private final String COOKIE_NAME = "SFCookie";
+
+  @Override
+  public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+    if(!validateUser(req)) {
+      res.setStatus(HttpServletResponse.SC_FORBIDDEN);
+      return;
+    }
+
+    HttpSession activeSession = req.getSession(false);
+    String userId = (String) activeSession.getAttribute("user_id");
+    Long noteId = Long.valueOf(req.getParameter("note_id"));
+    DataSource pool = (DataSource) req.getServletContext().getAttribute("my-pool");  
+
+    try (Connection conn = pool.getConnection()) {
+      favoriteNoteService.insertFavoriteNote(pool, noteId, userId);
+    } catch (SQLException ex) {
+      System.err.print(ex);
+    }
+  }
+
+  @Override
+  public void doDelete(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+    if(!validateUser(req)) {
+      res.setStatus(HttpServletResponse.SC_FORBIDDEN);
+      return;
+    }
+
+    HttpSession activeSession = req.getSession(false);
+    String userId = (String) activeSession.getAttribute("user_id");
+    Long noteId = Long.valueOf(req.getParameter("note_id"));
+    DataSource pool = (DataSource) req.getServletContext().getAttribute("my-pool");  
+
+    try (Connection conn = pool.getConnection()) {
+      favoriteNoteService.deleteRowByCompoundId(pool, noteId, userId);
+    } catch (SQLException ex) {
+      System.err.print(ex);
+    }
+  }
+
+  /**
+   * Validates the user using the request's session and cookies. If there is a
+   * valid user logged in, returns true. Otherwise, returns false.
+   **/
+  private boolean validateUser(HttpServletRequest req) {
+    Cookie[] cookies = req.getCookies();
+    String sessionId = null;
+    for (Cookie cookie : cookies) {
+      if (COOKIE_NAME.equals(cookie.getName())) {
+        sessionId = cookie.getValue();
+      }
+    }
+
+    // If there was no cookie passed, then auth has failed and user is not logged in
+    if(sessionId == null) {
+      return false;
+    }
+
+    HttpSession activeSession = req.getSession(false);
+    if (activeSession == null || activeSession.getAttribute("user_id") == null) {
+      return false;
+    }
+    return true;
+  }
+}
