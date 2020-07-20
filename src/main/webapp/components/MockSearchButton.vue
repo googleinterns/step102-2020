0b5f66b066ff2c15ff1e2a5d8d3f772042cf6c75
Andres David <andres.holguin@ufl.edu>
2020-07-20 15:54:07 -0400

    Create MockSearchButton.vue
    Create a component that acts like the searchbar, emitting mock note data for UI testing.

---
+++ src/main/webapp/components/MockSearchButton.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-btn @click="loadMock">Load Mock Search Results</v-btn>
+</template>
+
+<script>
+module.exports = {
+  data: function() {
+    return {
+      result: [
+        {
+          thumbnailSrc: "/assets/notes.png",
+          author: "Andres H.",
+          title: "A set of notes",
+          date: new Date(),
+          school: "A school",
+          course: "A course",
+          miscLabels: ["A", "B", "C"],
+          numDownloads: 123,
+          numFavorites: 456,
+          isFavorited: false,
+          pdfSource: "/assets/notes.pdf"
+        },
+        {
+          thumbnailSrc: "/assets/notes.png",
+          author: "Andres H.",
+          title: "A set of notes",
+          date: new Date(),
+          school: "A school",
+          course: "A course",
+          miscLabels: ["A", "B", "C"],
+          numDownloads: 123,
+          numFavorites: 456,
+          isFavorited: false,
+          pdfSource: "/assets/notes.pdf"
+        },
+        {
+          thumbnailSrc: "/assets/notes.png",
+          author: "Andres H.",
+          title: "A set of notes",
+          date: new Date(),
+          school: "A school",
+          course: "A course",
+          miscLabels: ["A", "B", "C"],
+          numDownloads: 123,
+          numFavorites: 456,
+          isFavorited: false,
+          pdfSource: "/assets/notes.pdf"
+        },
+      ]
+    }
+  },
+  methods: {
+    loadMock: function() {
+      this.$emit('searchresult', this.result)
+    }
+  }
+}
+</script>
