39ee66b2c0d448596a289b792614180aa494d663
Keely Wan <keelywan00@gmail.com>
2020-07-21 15:44:41 +0000

    Add DataSource import to download note servlet

--- src/main/java/com/google/starfish/servlets/DownloadNoteServlet.java
+++ src/main/java/com/google/starfish/servlets/DownloadNoteServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;  
 import javax.servlet.http.HttpServletRequest;  
 import javax.servlet.http.HttpServletResponse;  
+import javax.sql.DataSource;
 import com.google.starfish.services.NoteService;
 
 /** Servlet that increments the number of downloads on a note. */
