ef1537239486c033d790d72303106800ceb6ceb7
Andres David <andres.holguin@ufl.edu>
2020-07-21 18:28:38 -0400

    NoteGridCollection: Abstract out filter and compare functions.
    Rename the way notes are computed, simplifying by moving filters and sort comparators to methods.

--- src/main/webapp/components/NoteGridCollection.vue
+++ src/main/webapp/components/NoteGridCollection.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="note-grid-collection">
-    <note-grid-card v-for="(note, index) in sortedNotes"
     <v-card-title>{{header}}</v-card-title>
+
+    <note-grid-card v-for="(note, index) in notes"
                     :key="index"
                     v-bind="note"
                     @click="onClick(note)">
@@ -15,7 +16,7 @@ module.exports = {
     'note-grid-card': httpVueLoader('/components/NoteGridCard.vue'),
   },
   props: {
-    notes: {
+    noteData: {
       type: Array,
       required: true,
     },
@@ -24,16 +25,11 @@ module.exports = {
     header: String,
   },
   computed: {
-    activeNotes: function() {
-      // Filter the notes that get shown based on filter provided by parent.
-      if (this.filters && this.filters.length) {
-        return this.notes.filter(this.noteFilter);
-      }
-      return this.notes;
-    },
-    sortedNotes: function() {
-      // Sort the currently active notes using sortFunc for comparisons.
-      return this.activeNotes.sort(this.compareFunc);
+    notes: function() {
+      // Filter by date, matching terms, and sort with compareFunc
+      return this.noteData
+        .filter(this.noteFilter)
+        .sort(this.compareFunc);
     }
   },
   methods: {
@@ -41,13 +37,17 @@ module.exports = {
       this.$parent.$emit('open-preview', note)
     },
     noteFilter: function(note) {
-      // True if some filter matches a miscLabel, title, or school, or course
+      // If no filter or empty filter, immediately pass filter.
+      if (!this.filters || !this.filters.length) return true;
+
+      // Pass if some filter matches a miscLabel, title, or school, or course
       return this.filters.some(filter => {
         return note.miscLabels.includes(filter)
           || note.title.includes(filter)
           || note.school.includes(filter)
           || note.course.includes(filter)
       });
+    },
     }
   },
 }
