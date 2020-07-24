1c154e8135278554863eecfc1ed452e7716a19ce
Aradhya Bansal <aradhyabansal2000@gmail.com>
2020-07-24 20:30:05 +0000

    Remove unused import

--- src/main/java/com/google/starfish/services/FavoriteNoteService.java
+++ src/main/java/com/google/starfish/services/FavoriteNoteService.java
@@ -8,7 +8,6 @@ import java.sql.Date;
 import javax.sql.DataSource;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.Calendar;
 import com.google.starfish.models.Note;
 
