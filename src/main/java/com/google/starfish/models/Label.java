d8a4ad5c05462fa9da6bc4d199309240929908bc
Aradhya Bansal <aradhyabansal2000@gmail.com>
2020-07-28 12:33:44 +0000

    Alter enum declaration for type and add member getters to Label model

--- src/main/java/com/google/starfish/models/Label.java
+++ src/main/java/com/google/starfish/models/Label.java
@@ -4,9 +4,19 @@ import java.util.Objects;
 
 /** Enum declaration for type of label. */
 enum Type {
-  SCHOOL,
-  COURSE,
-  MISC
+  SCHOOL("School"),
+  COURSE("Course"),
+  MISC("Misc");
+
+  private final String type;
+
+  private Type(String type) {
+    this.type = type;
+  }
+
+  public String getType() {
+    return this.type;
+  }
 }
 
 /** Represents a label */
@@ -15,8 +25,16 @@ public class Label {
   private String title;
   private Type type;
 
-  public Label(String title, Type type) {
+  public Label(String title, String type) {
     this.title = Objects.requireNonNull(title);
-    this.type = Objects.requireNonNull(type);
+    this.type = Objects.requireNonNull(Type.valueOf(type));
+  }
+
+  public String getTitle() {
+    return this.title;
+  }
+
+  public String getType() {
+    return type.getType();
   }
 }
