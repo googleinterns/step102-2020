9acd15140165e641eadaa4419f8cea8ed5d74de4
Keely Wan <keelywan00@gmail.com>
2020-07-30 19:38:55 +0000

    Center Please Sign In text and change margins

--- src/main/webapp/views/ProfilePage.vue
+++ src/main/webapp/views/ProfilePage.vue
@@ -1,13 +1,19 @@
 <template>
   <v-app>
     <navbar @setuser="onSetUser" @signedin="onSignIn"></navbar>
-    <div v-if="!signedIn">
-      <h1>Please sign in</h1>
-    </div> 
+    <v-container v-if="!signedIn"
+                 class="fill-height"
+                 fluid>
+      <v-row align="center" justify="center">
+        <h1 class="lg-heading">Please sign in</h1>
+      </v-row>
+    </v-container>
     <div v-else> 
-        <profile-header :points="user.points"
-                        :name="user.displayName">
-        </profile-header>
+      <profile-header :points="user.points"
+                      :name="user.displayName"
+                      :display-picture="user.displayPicture">
+      </profile-header>
+      <div class="notes-container">
         <note-grid>
           <note-grid-collection :note-data="user.favoriteNotes"
                                 header="Favorited Notes"
@@ -18,7 +24,8 @@
                                 :compare-func="descDate">
           </note-grid-collection>
         </note-grid>
-     </div>
+      </div>
+    </div>
   </v-app>
 </template>
 
@@ -35,6 +42,7 @@ module.exports = {
       user: {
         points: 0,
         displayName: "Default Username",
+        displayPicture: "Default Picture"
       },
       signedIn: false,
       descFavorites: function(noteA, noteB) {
@@ -47,6 +55,7 @@ module.exports = {
   },
   methods: {
     onSetUser: function(user) {
+      console.log(user);
       this.user = user;
     },
     onSignIn: function(signedIn) {
@@ -55,3 +64,13 @@ module.exports = {
   }
 }
 </script>
+
+<style>
+.lg-heading {
+  font: 6rem 'Caveat Brush';
+}
+
+.notes-container {
+  margin: 25px;
+}
+</style>
